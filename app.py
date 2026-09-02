01428ffb236d9b739bdddc7b29f06b2eb55d179a
Sobin <sobincse33@gmail.com>
2026-09-02 15:48:45 +0530

    feature -salary change

--- app.py
+++ app.py
@@ -2,8 +2,4 @@ print("Employee Management Application")
 
 employee_name = "Sobin"
 employee_role = "Data Engineer"
-employee_salary = 75000
-
-print("Name:", employee_name)
-print("Role:", employee_role)
-print("Salary:", employee_salary)
+e
