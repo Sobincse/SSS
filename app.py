9e78c1c5a0a32135f934093c98b73c1cdbc65bea
Sobin <sobincse33@gmail.com>
2026-09-02 15:20:27 +0530

    add new data

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-dsdfdf
+print("Employee Management Application")
+
+employee_name = "Sobin"
+employee_role = "Data Engineer"
+employee_salary = 75000
+
+print("Name:", employee_name)
+print("Role:", employee_role)
+print("Salary:", employee_salary)
